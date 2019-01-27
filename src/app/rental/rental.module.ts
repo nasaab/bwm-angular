abe0177d84f0d906c520da22ce3e2ab52ae19fcc
Akshay Chothe <akshay280294@gmail.com>
2019-01-27 13:56:36 +0530

    rental folder created for list rental and item rental, and shared folder created for service layer

---
+++ src/app/rental/rental.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RentalComponent } from './rental.component';
+import { RentalListComponent } from './rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
+
+import { RentalService } from '../rental/shared/rental.service';
+
+@NgModule({
+    declarations: [
+        RentalComponent, RentalListComponent, RentalListItemComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    providers: [RentalService]
+})
+ 
+export class RentalModule {
+
+}
