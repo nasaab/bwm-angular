90b830825228a2ff94c08908eaeea364da710240
Akshay Chothe <akshay280294@gmail.com>
2019-02-21 23:37:55 +0530

    Added rental creation and search rental routes

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Rental = require('../model/rental');
+const User = require('../model/user');
 const UserController = require('../controllers/user');
+const MongooseHelpers = require('../helpers/mongoose');
 
 router.get('/secret', UserController.authMiddleware,  function(req, res){
     res.json({"secret": true});
-});
-
-router.get('', function(req, res) {
-    //res.json({'Ok': true});
-    Rental.find({})
-           .select('-bookings')
-           .exec(function(err, foundRentals) {
-        res.json(foundRentals);
-    });
 });
 
 router.get('/:id', function(req, res) {
@@ -24,10 +17,56 @@ router.get('/:id', function(req, res) {
           .populate('bookings', 'startAt endAt -_id')
           .exec(function(err, foundRental) {
         if(err) {
-            return res.status(422).send({errore: {title: 'Rental Error', detail: 'Could not fund Rental'}});
+            return res.status(422).send({error: {title: 'Rental Error', detail: 'Could not fund Rental'}});
         }
         return res.json(foundRental);
     });  
 });
+
+router.post('', UserController.authMiddleware, function(req, res) {
+    const { title, city, street, category, image, shared, bedrooms, description, dailyRate } = req.body;
+    console.log(req.body);
+    const user = res.locals.user;
+    let rental = new Rental({title, city, street, category, image, shared, bedrooms, description, dailyRate});
+    rental.user = user;
+
+    Rental.create(rental, function(err, newRental) {
+        if(err) {
+            console.log(err);
+            return res.status(422).send({error: MongooseHelpers.normalizeErrors(err.errors)});
+        }
+
+        User.update({_id: user.id}, { $push: {rentals: newRental} }, function(){});
+
+        return res.json(newRental);
+    })
+});
+
+router.get('', function(req, res) {
+    const city = req.query.city;
+    const query = city ? {city: city.toLowerCase()} : {};
+
+    Rental.find(query)
+    .select('-bookings')
+    .exec(function(err, foundRentals) {
+        if(err) {
+            return res.status(422).send({error: MongooseHelpers.normalizeErrors(err.errors)}); 
+        }
+
+        if(city && foundRentals.length === 0) {
+            return res.status(422).send({error: {title: 'No Rentals Found', detail: 'There are no rentals available for city ${city}'}});
+        }
+        return res.json(foundRentals);
+    });
+});
+
+// router.get('', function(req, res) {
+//     //res.json({'Ok': true});
+//     Rental.find({})
+//            .select('-bookings')
+//            .exec(function(err, foundRentals) {
+//         res.json(foundRentals);
+//     });
+// });
 
 module.exports = router;
